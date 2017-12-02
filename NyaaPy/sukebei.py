0611fdb0fb539b584dab0ff4de5f3812879eb51c
JuanjoSalvador <juanjosalvador@openmailbox.org>
2017-12-02 16:47:29 +0100

    Added support for Sukebei at Nyaa.si

--- NyaaPy/sukebei.py
+++ NyaaPy/sukebei.py
@@ -2,9 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 from NyaaPy.utils import Utils as utils
 
-class Nyaa:
-    URI = "http://nyaa.si"
-
+class SukebeiNyaa:
     def search(keyword, **kwargs):
         category = kwargs.get('category', 0)
         subcategory = kwargs.get('subcategory', 0)
@@ -12,9 +10,9 @@ class Nyaa:
         page = kwargs.get('page', 0)
 
         if page > 0:
-            r = requests.get("{}/?f={}&c={}_{}&q={}&p={}".format(Nyaa.URI, filters, category, subcategory, keyword, page))
+            r = requests.get("{}/?f={}&c={}_{}&q={}&p={}".format("http://sukebei.nyaa.si", filters, category, subcategory, keyword, page))
         else:
-            r = requests.get("{}/?f={}&c={}_{}&q={}".format(Nyaa.URI, filters, category, subcategory, keyword))
+            r = requests.get("{}/?f={}&c={}_{}&q={}".format("http://sukebei.nyaa.si", filters, category, subcategory, keyword))
 
         soup = BeautifulSoup(r.text, 'html.parser')
         rows = soup.select('table tr')
@@ -22,21 +20,21 @@ class Nyaa:
         return utils.parse_nyaa(rows, limit=None)
 
     def get(id):
-        r = requests.get("{}/view/{}".format(Nyaa.URI, id))
+        r = requests.get("http://sukebei.nyaa.si/view/{}".format(id))
         soup = BeautifulSoup(r.text, 'html.parser')
         content = soup.findAll("div", { "class": "panel", "id": None})
         
         return utils.parse_single(content)
 
     def get_user(username):
-        r = requests.get("{}/user/{}".format(Nyaa.URI, username))
+        r = requests.get("http://sukebei.nyaa.si/user/{}".format(username))
         soup = BeautifulSoup(r.text, 'html.parser')
 
         return utils.parse_nyaa(soup.select('table tr'), limit=None)
 
     def news(number_of_results):
-        r = requests.get(Nyaa.URI)
+        r = requests.get("http://sukebei.nyaa.si/")
         soup = BeautifulSoup(r.text, 'html.parser')
         rows = soup.select('table tr')
 
-        return utils.parse_nyaa(rows, limit=number_of_results + 1)
+        return utils.parse_sukebei(rows, limit=number_of_results + 1)
