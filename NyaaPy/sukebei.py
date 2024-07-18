43c42da8364c173b29e5436a9c94967844c261a5
Juanjo Salvador <juanjosalvador@netc.eu>
2024-07-18 12:09:05 +0200

    Delete NyaaPy directory

--- NyaaPy/sukebei.py
+++
@@ -1,8 +0,0 @@
-from NyaaPy.nyaa import Nyaa
-from NyaaPy import utils
-
-class SukebeiNyaa(Nyaa):
-
-    def __init__(self):
-        self.SITE = utils.TorrentSite.SUKEBEINYAASI
-        self.URL = utils.TorrentSite.SUKEBEINYAASI.value
